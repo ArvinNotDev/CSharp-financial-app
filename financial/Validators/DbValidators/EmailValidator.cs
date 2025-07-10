3122b95c1c685d2b5735b128edf82a7879369874
ArvinNotDev <arveenjafari19@gmail.com>
2025-07-10 11:26:09 +0330

    new database validator added

---
+++ financial/Validators/DbValidators/EmailValidator.cs
@@ -0,0 +1,21 @@
+﻿using financial.Services;
+using financial.Validators.FieldValidators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace financial.Validators.DbValidators
+{
+    internal static class EmailUniqueValidator
+    {
+        public static ValidationResult Validate(string email)
+        {
+            if (AccountManager.EmailExists(email))
+                return ValidationResult.Fail(ValidationMessages.EmailAlreadyExists);
+
+            return ValidationResult.Success();
+        }
+    }
+}
