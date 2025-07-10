5e893b217b4ea76702830d4ebef23c8097b99950
ArvinNotDev <arveenjafari19@gmail.com>
2025-07-10 11:01:30 +0330

    modular Validation message for future translations

---
+++ financial/Validators/ValidationMessages.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace financial.Validators
+{
+    internal class ValidationMessages
+    {
+        public const string FirstNameRequired = "First name cannot be empty.";
+        public const string LastNameRequired = "Last name cannot be empty.";
+        public const string EmailInvalid = "Invalid email address.";
+        public const string BirthYearInvalid = "Birth year is out of valid range.";
+        public const string PasswordTooShort = "Password must be at least 6 characters long.";
+        public const string EmailAlreadyExists = "This email is already registered.";
+    }
+}
