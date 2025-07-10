72201cdbb2457059018d4cad308ac26c7137a7ad
ArvinNotDev <arveenjafari19@gmail.com>
2025-07-10 11:04:48 +0330

    Validators are now fully functional :)

--- financial/Validators/AccountValidator.cs
+++ financial/Validators/AccountValidator.cs
@@ -1,12 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace financial.Validators
 {
-    internal class AccountValidator
+    internal static class AccountValidator
     {
+        public static ValidationResult Validate(
+            string firstName,
+            string lastName,
+            string email,
+            DateTime? birthDate,
+            bool? gender,
+            string password)
+        {
+            if (string.IsNullOrWhiteSpace(firstName))
+                return ValidationResult.Fail(ValidationMessages.FirstNameRequired);
+
+            if (string.IsNullOrWhiteSpace(lastName))
+                return ValidationResult.Fail(ValidationMessages.LastNameRequired);
+
+            if (string.IsNullOrWhiteSpace(email) || !email.Contains("@") || !email.Contains("."))
+                return ValidationResult.Fail(ValidationMessages.EmailInvalid);
+
+            if (birthDate == null)
+                return ValidationResult.Fail(ValidationMessages.BirthYearInvalid);
+
+            if (birthDate > DateTime.Now)
+                return ValidationResult.Fail(ValidationMessages.BirthYearInvalid);
+
+            if (birthDate.Value.Year < 1900)
+                return ValidationResult.Fail(ValidationMessages.BirthYearInvalid);
+
+            if (string.IsNullOrWhiteSpace(password) || password.Length < 6)
+                return ValidationResult.Fail(ValidationMessages.PasswordTooShort);
+
+            return ValidationResult.Success();
+        }
     }
 }
