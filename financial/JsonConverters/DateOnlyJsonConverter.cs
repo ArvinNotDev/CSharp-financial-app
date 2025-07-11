d7d8a39feb9cd16b5ff86f56824bafae27ff36c6
ArvinNotDev <arveenjafari19@gmail.com>
2025-07-11 17:18:51 +0330

    new json converter for serialization

---
+++ financial/JsonConverters/DateOnlyJsonConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace financial.JsonConverters
+{
+    public class DateOnlyJsonConverter : JsonConverter<DateTime>
+    {
+        private const string Format = "yyyy-MM-dd";
+
+        public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            var dateString = reader.GetString();
+            if (DateTime.TryParseExact(dateString, Format, CultureInfo.InvariantCulture, DateTimeStyles.None, out var date))
+            {
+                return date;
+            }
+
+            throw new JsonException($"Could not parse '{dateString}' to DateTime with format '{Format}'");
+        }
+
+        public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(value.ToString(Format));
+        }
+    }
+}
