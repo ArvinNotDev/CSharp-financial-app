ba8b330f7c2fc083d9e45b5ec371940d96d0453c
ArvinNotDev <Arveenjafari19@gmail.com>
2025-07-13 13:11:21 +0330

    adding product category enum

---
+++ financial/Enums/ProductCategory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace financial.Enums
+{
+    public enum ProductCategory
+    {
+        Electronics,
+        Grocery,
+        Clothing,
+        Services,
+        Other
+    }
+}
