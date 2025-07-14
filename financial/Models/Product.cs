a29acee80844f4e574902d4717d768a7d8143e7c
ArvinNotDev <arveenjafari19@gmail.com>
2025-07-14 11:24:50 +0330

    product.cs fixed

--- financial/Models/Product.cs
+++ financial/Models/Product.cs
@@ -1,18 +1,11 @@
-﻿using financial.Enums;
-using financial.Transactions;
+﻿using financial.Transactions;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Linq;
 
 namespace financial.Models
 {
     internal class Product
     {
         public string id { get; set; } = Guid.NewGuid().ToString();
-        public string accountId { get; set; }
         public string name { get; set; }
         public string description { get; set; }
         public decimal price { get; set; }
@@ -20,15 +13,14 @@ namespace financial.Models
         public DateTime CreatedDate { get; } = DateTime.UtcNow;
         public DateTime? LastModifiedDate { get; set; }
 
-        public Product(string accountId, string name, string description, decimal price, TransactionType transactionType)
+        public Product(string name, string description, decimal price, TransactionType transactionType)
         {
-            this.accountId = accountId;
             this.name = name;
             this.description = description;
             this.price = price;
             this.transactionType = transactionType;
-
         }
+
         public override string ToString() => id;
     }
 }
